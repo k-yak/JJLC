1d28e745eb7331868c3de968e22531ef6aa5c213
k-yak <florian.rotagnon@gmail.com>
2015-02-23 15:50:38 +0100

    add demo page

--- scripts/main.js
+++ scripts/main.js
@@ -1,77 +1,84 @@
 (function (jjlc) {
-    "use strict";
+    "use strict";   
+    var bCompress = document.getElementById('compress');
+    var bDecompress = document.getElementById('decompress');
+    var bFull = document.getElementById('full');
+    var json = document.getElementById('json');
+    var smallJson = document.getElementById('small-json');
+    var bigJson = document.getElementById('big-json');
+    var inter = document.getElementById('inter');
+    var results = document.getElementById('results');
+    var big = document.getElementById('big');
+    var small = document.getElementById('small');
     
-    var tst_s = '{' +
-        '"$schema": "http://json-schema.org/draft-04/schema#",' +
-        '"title": "Product",'  +
-        '"description": "A product from Acme\'s catalog",'  +
-        '"type": "object",'  +
-        '"products": [{'  +
-        '    "id": {'  +
-        '        "description": "The unique identifier for a product",'  +
-        '        "type": "integer"'  +
-        '    },'  +
-        '    "name": {'  +
-        '        "description": "Name of the product",'  +
-        '        "type": "string"'  +
-        '    },'  +
-        '    "price": {'  +
-        '        "type": "number",'  +
-        '        "minimum": 0,'  +
-        '        "exclusiveMinimum": true'  +
-        '    },'  +
-        '    "tags": {'  +
-        '        "type": "array",'  +
-        '        "items": {'  +
-        '            "type": "string"'  +
-        '        },'  +
-        '        "minItems": 1,'  +
-        '        "uniqueItems": true'  +
-        '    }'  +
-        '},{'  +
-        '    "id": {'  +
-        '        "description": "The unique identifier for a product",'  +
-        '        "type": "integer"'  +
-        '    },'  +
-        '    "name": {'  +
-        '        "description": "Name of the product",'  +
-        '        "type": "string"'  +
-        '    },'  +
-        '    "price": {'  +
-        '        "type": "number",'  +
-        '        "minimum": 0,'  +
-        '        "exclusiveMinimum": true'  +
-        '    },'  +
-        '    "tags": {'  +
-        '        "type": "array",'  +
-        '        "items": {'  +
-        '            "type": "string"'  +
-        '        },'  +
-        '        "minItems": 1,'  +
-        '        "uniqueItems": true'  +
-        '    }'  +
-        '}],'  +
-        '"required": ["id", "name", "price"]'  +
-        '}';    
     
-    var l = tst_s.length;
-    var start = new Date().getTime();
-    console.log('COMPRESS');
-    var compressed = JJLC.setItem('testStr', tst_s);
-
-    var dict = JSON.stringify(JJLC.getDict('testStr'));    
-    var size = compressed.length + dict.length;
-
-    console.log('initial size : ' + l + ' c');
-    console.log('compressed size : ' + size + ' c');
-    console.log('win : ' + (l - size) + ' c');
-    console.log('win : ' + ((l - size) / l * 100).toPrecision(3) + '%');
-    console.log('DECOMPRESS');
+    function compress() {
+        var l = json.value.length;
+        results.value = 'COMPRESS ONLY\n';
+        
+        var start = new Date().getTime();
+        var compressed = JJLC.setItem('testStr', json.value);
+        var end = new Date().getTime();
+        var time = end - start;
+        
+        inter.value = compressed;
+        
+        var size = compressed.length + JSON.stringify(JJLC.getDict('testStr')).length;
+        results.value += 'initial size : ' + (l / 1024).toPrecision(3) + ' kB\n';
+        results.value += 'compressed size : ' + (size / 1024).toPrecision(3) + ' kB\n';
+        results.value += 'win : ' + ((l - size) / 1024).toPrecision(3) + ' kB\n';
+        results.value += 'win : ' + ((l - size) / l * 100).toPrecision(3) + '%\n';
+        results.value += 'execution : ' + time + ' milliseconds\n';
+    }
     
-    var decompressed = JJLC.getItem('testStr');
-    var end = new Date().getTime();
-    var time = end - start;
-    console.log('execution : ' + time + ' milliseconds');
+    function decompress() {
+        var l = json.value.length;
+        results.value = '';
+        results.value = 'DECOMPRESS ONLY\n';
+        
+        var start = new Date().getTime();
+        var decompressed = JJLC.getItem('testStr');
+        var end = new Date().getTime();
+        var time = end - start;
+        
+        inter.value = decompressed;
+        
+        results.value += 'execution : ' + time + ' milliseconds\n';
+    }
     
-    //console.log(decompressed);
+    function full() {
+        var l = json.value.length;
+        results.value = 'COMPRESS AND DECOMPRESS\n';
+        
+        var start = new Date().getTime();
+        var compressed = JJLC.setItem('testStr', json.value);
+        var decompressed = JJLC.getItem('testStr');
+        var end = new Date().getTime();
+        var time = end - start;
+        
+        inter.value = decompressed;
+        
+        var size = compressed.length + JSON.stringify(JJLC.getDict('testStr')).length;
+        results.value += 'initial size : ' + (l / 1024).toPrecision(3) + ' kB\n';
+        results.value += 'compressed size : ' + (size / 1024).toPrecision(3) + ' kB\n';
+        results.value += 'win : ' + ((l - size) / 1024).toPrecision(3) + ' kB\n';
+        results.value += 'win : ' + ((l - size) / l * 100).toPrecision(3) + '%\n';
+        results.value += 'execution : ' + time + ' milliseconds\n';
+    }
+    
+    bCompress.onclick = compress;
+    
+    bDecompress.onclick = decompress;
+     
+    bFull.onclick = full;
+    
+    small.onclick = function () {
+        json.value = smallJson.value;  
+    };
+    
+    big.onclick = function () {
+        json.value = bigJson.value;  
+    };
+    
+    json.value = smallJson.value; 
 }());
